ec5a0dca524a83b7cc6b2b71b8b6c62aff155c96
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2017-06-14 16:48:25 +0200

    Minimal working test for the Tahoma box

--- demos/tahomaTest.py
+++ demos/tahomaTest.py
@@ -1,15 +1,18 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 import json
 import tahoma.protocol
 
-
 creds = open('.tahoma.credentials').read()
-print('Parsing credentials')
 creds = json.loads(creds)
 print('Logging in to Tahomalink')
-tahoma = tahoma.protocol.Protocol(creds['USERNAME'], creds['PASSWORD'])
+tahoma = tahoma.protocol.Protocol(creds['USERNAME'], creds['PASSWORD'], 'tahomacookie')
+tahoma.getSetup()
 
-print(tahoma.getUser())
-#print(tahoma.getSetup())
-#print(tahoma.getDevices())
+#print('User:', tahoma.getUser())
+
+print('All registered devices')
+devs = tahoma.getDevices()
+for d in devs.keys():
+	print('Device %s: type = %s, label = %s' % (d, devs[d].type, devs[d].label))
+
