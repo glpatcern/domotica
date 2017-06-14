264d3648e935a9fbe9e400801d33794dcd02ad79
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2017-06-14 18:34:31 +0200

    More display for the Tahoma box

--- demos/tahomaTest.py
+++ demos/tahomaTest.py
@@ -12,7 +12,20 @@ tahoma.getSetup()
 #print('User:', tahoma.getUser())
 
 print('All registered devices')
+devbylabel = {}
 devs = tahoma.getDevices()
 for d in devs.keys():
-	print('Device %s: type = %s, label = %s' % (d, devs[d].type, devs[d].label))
+    # build reverse dictionary by label
+    devbylabel[devs[d].label] = devs[d]
+    # print info
+    print('Device %s: type = %s, label = %s' % (d, devs[d].type, devs[d].label))
+    print('    commands = %s' % devs[d].commandDefinitions)
+    try:
+        print('    state = %s' % devs[d].activeStates)
+    except AttributeError:
+        pass
+
+for ag in tahoma.getActionGroups():
+	for a in ag.actions:
+	    print('For device %s, command = %s' % (devs[a.deviceURL].label, a.commands))
 
