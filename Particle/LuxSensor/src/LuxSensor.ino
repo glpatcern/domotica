5284471dc2f2f2ddeb320760c9f831f62e0c4a23
Giuseppe Lo Presti <glopresti@gmail.com>
2017-06-28 16:52:37 +0200

    Lux sensor only using the Particle API

--- Particle/LuxSensor/src/LuxSensor.ino
+++ Particle/LuxSensor/src/LuxSensor.ino
@@ -5,30 +5,18 @@
  *
  * Changelog:
  * v1.0 - 22-06-2017: initial fork from THDweeter to experiment with analogRead
+ * v2.0 - 28-06-2017: use the Particle cloud API instead of dweet.io
  ******************************************************************************/
 
-// This #include statement was automatically added by the Particle IDE.
-#include <HttpClient.h>
-
-int photoPin = A5;                      // photoresistor pin
+int photoPin = A5;           // photoresistor pin
 int powerPin = A0;
-String thingName = "glp_photon_lux";    // thing name for Dweeter
-
-HttpClient http;
-// Headers currently need to be set at init, useful for API keys etc.
-http_header_t headers[] = {
-  //  { "Content-Type", "application/json" },
-  //  { "Accept" , "application/json" },
-  { "Accept" , "*/*"},
-  { NULL, NULL }  // NOTE: Always terminate headers will NULL
-};
 float lux, lmin, lmax;
 int day = 0;
 
 
 void setup() {
   Time.zone(+2);     // set your time zone if you wish a local time display
-  pinMode(photoPin, INPUT);       // prepare the photoresistor
+  pinMode(photoPin, INPUT);     // prepare the photoresistor
   pinMode(powerPin, OUTPUT);
   digitalWrite(powerPin, HIGH);
 }
@@ -53,8 +41,6 @@ int getAndStoreValues() {
 
 
 int doReadout(bool button) {
-  http_request_t request;
-  http_response_t response;
   // get and store current values
   if(getAndStoreValues()) {
     // failed to read out, try again right away
@@ -70,12 +56,9 @@ int doReadout(bool button) {
       // also publish daily max and min values when button pressed
       (button ? "&lmin=" + slmin + "&lmax=" + slmax : "") +
       "&timestamp=" + now + "&time=" + Time.format(now, "%Y-%m-%dT%H:%M:%S");
-  // post to dweet: follow at http://dweet.io/follow/thingName
-  request.hostname = "dweet.io";
-  request.port = 80;
-  request.path = "/dweet/for/" + thingName + "?" + payload;
-  http.get(request, response, headers);
-  //Particle.publish("Lux", sl);
+  if(!Particle.publish("Lux", payload, PRIVATE, WITH_ACK)) {
+    return -1;
+  }
   return 0;
 }
 
