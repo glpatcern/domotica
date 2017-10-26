7179eea535a18b936b6b81db8879af6cd6dfe552
Giuseppe Lo Presti <glopresti@gmail.com>
2017-10-26 10:07:38 +0200

    Added header to script

--- homeass/qnapsh/regen_docker_hass.sh
+++ homeass/qnapsh/regen_docker_hass.sh
@@ -1,10 +1,22 @@
+#!/bin/sh
+#
+# regen_docker_hass.sh
+#
+# regenerates the home assistant docker image by pulling it
+# from the repo and recreating and restarting a container for it
+#
+# Giuseppe Lo Presti, September 2017
+################################################################
+
 currimg=`docker images | grep homeassistant | awk '{print $3}'`
 currcont=`docker ps | grep homeass | awk '{print $1}'`
 docker stop $currcont
+
 echo === Pulling new docker image ===
 docker pull homeassistant/home-assistant
 echo === Removing previous image ===
 docker rmi -f $currimg && docker rm $currcont
 echo === Starting a new container with the new image ===
-docker run -d -p 8123:8123 --name="home-assistant" -v /share/Container/homeass-config:/config --net=host  --restart=always --privileged homeassistant/home-assistant
+docker run -d -p 8123:8123 --name="home-assistant" -v /share/Container/homeass-config:/config \
+           --net=host  --restart=always --privileged homeassistant/home-assistant
 
