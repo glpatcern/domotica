46a2ceed9bd82f0476eff3b6bcca1f6faaf83f8e
Giuseppe Lo Presti <glopresti@gmail.com>
2017-12-27 17:13:00 +0100

    Mounted the IOMega disk inside the homeass docker and added the systemmonitor sensor to monitor its free space

--- homeass/qnapsh/regen_docker_hass.sh
+++ homeass/qnapsh/regen_docker_hass.sh
@@ -13,10 +13,11 @@ currcont=`docker ps | grep homeass | awk '{print $1}'`
 docker stop $currcont
 
 echo === Pulling new docker image ===
-docker pull homeassistant/home-assistant
-echo === Removing previous image ===
-docker rmi -f $currimg && docker rm $currcont
+docker pull homeassistant/home-assistant | grep Status | grep -v 'up to date' && \
+  echo === Removing previous image === && \
+  docker rmi -f $currimg
+docker rm $currcont
 echo === Starting a new container with the new image ===
-docker run -d -p 8123:8123 --name="home-assistant" -v /share/Container/homeass-config:/config \
-           --net=host  --restart=always --privileged homeassistant/home-assistant
+docker run -d -p 8123:8123 --name="home-assistant" --privileged -v /share/Container/homeass-config:/config \
+       -v /mnt/rf/nd/0/0407ca651ddb04329464fd677429486f816b7766:/mnt --net=host --restart=always homeassistant/home-assistant
 
