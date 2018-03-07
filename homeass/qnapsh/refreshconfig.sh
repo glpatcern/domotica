a312508e87149af5deca84b29d87becc3ac91273
Giuseppe Lo Presti <glopresti@gmail.com>
2018-03-07 23:03:58 +0100

    Refresh the SSL certificate also on the Grafana docker

--- homeass/qnapsh/refreshconfig.sh
+++ homeass/qnapsh/refreshconfig.sh
@@ -14,9 +14,11 @@ while true; do
   inotifywait --event modify ~glp/QSync/dev/DOMOTICA/homeass/*yaml ~glp/QSync/dev/DOMOTICA/homeass/automations/*
   cp ~glp/QSync/dev/DOMOTICA/homeass/*yaml /share/Container/homeass-config
   cp ~glp/QSync/dev/DOMOTICA/homeass/automations/* /share/Container/homeass-config/automations
-  # also refresh the SSL certificate
+  # also refresh the SSL certificate for HA and Grafana
   cp /mnt/HDA_ROOT/.config/QcloudSSLCertificate/cert/cert /share/Container/homeass-config/sslcert.crt
   cp /mnt/HDA_ROOT/.config/QcloudSSLCertificate/cert/key /share/Container/homeass-config/sslkey.key
+  cp /mnt/HDA_ROOT/.config/QcloudSSLCertificate/cert/cert /share/Container/grafana-storage/sslcert.crt
+  cp /mnt/HDA_ROOT/.config/QcloudSSLCertificate/cert/key /share/Container/grafana-storage/sslkey.key
   # if running, restart the container to reflect the change
   id=`docker ps | grep homeass | awk '{print $1}'` && \
     docker restart $id
