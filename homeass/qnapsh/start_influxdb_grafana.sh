295dae1ad21ee8ce3baabde8dd237d12369ce3f6
Giuseppe Lo Presti <glopresti@gmail.com>
2018-11-30 21:37:29 +0100

    Fixed script for grafana docker

--- homeass/qnapsh/start_influxdb_grafana.sh
+++ homeass/qnapsh/start_influxdb_grafana.sh
@@ -2,7 +2,7 @@
 #
 # start the docker container for InfluxDB and Grafana
 
-docker run -d --name influxdb-grafana \
+docker run -d --restart=always --name influxdb-grafana \
   -p 3003:3003 -p 3004:8083 -p 8086:8086 -p 22022:22 \
   -v /share/Container/influxdb-storage:/var/lib/influxdb \
   -v /share/Container/grafana-storage:/var/lib/grafana \
