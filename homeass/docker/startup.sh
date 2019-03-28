869e86fe76c981caa95c80a561cd1f7bad62e0bf
Giuseppe Lo Presti <glopresti@gmail.com>
2019-03-28 12:44:38 +0100

    Improved docker startup script

--- homeass/docker/startup.sh
+++ homeass/docker/startup.sh
@@ -6,8 +6,15 @@
 ########################################################
 
 # make sure influxDB is up
+n=0
 while ! wget -q http://localhost:3004 -O /dev/null; do
   sleep 10
+  let "n=n+1"
+  if [ "$n" -eq 60 ]; then
+    # after (and every) 10 mins attempt to restart it
+    ssh hass@naslopresti ./qnap_restart_influx.sh
+    let "n=0"
+  fi
 done
 
 # make sure the IOMega disk is mounted, but give up after 5 mins
